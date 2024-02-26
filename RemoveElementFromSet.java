8af1544157e857e13958bfd9f51ab89fe79e9c07
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-02-26 11:53:31 +0400

    removing an element from a Set which has integers values

---
+++ RemoveElementFromSet.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveElementFromSet {
+    public static void main(String[] args){
+        Set<Integer> integerSet=new HashSet<>();// decleare set
+        // add the elements to set
+        integerSet.add(1);
+        integerSet.add(2);
+        integerSet.add(3);
+        integerSet.add(4);
+        integerSet.add(5);
+
+        System.out.println("Set Elements before remove 3 : "+integerSet);//print set elements
+        integerSet.remove(3);
+        System.out.println("Set Elements after remove 3 : "+integerSet);//print set elements
+    }
+}
