d9fc7d24262570b58ba065b325bc430ba60698ec
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-02-26 10:14:36 +0400

    using java code to Create a Set of integers and add the numbers 1, 2, 3, 4, and 5 to it. Then, print the Set to the console.

---
+++ CreateAndPopulateSet.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CreateAndPopulateSet {
+    public static void main(String[] args){
+        Set<Integer> integerSet=new HashSet<>();// decleare set
+        // add the elements to set
+        integerSet.add(1);
+        integerSet.add(2);
+        integerSet.add(3);
+        integerSet.add(4);
+        integerSet.add(5);
+        System.out.println("Set Elements: "+integerSet);//print set elements
+    }
+}
