65f29114c8ec9deafa2402af7bf0260ee491acad
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-02-27 13:39:16 +0400

    Use a Set to find the unique elements in a list and their frequencies.

--- UniqueElementsFrequency.java
+++ UniqueElementsFrequency.java
@@ -1,29 +1,22 @@
 import java.util.*;
 
 public class UniqueElementsFrequency {
+    public static List<Integer> numbersList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 1, 8, 3, 7, 2,1, 2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 1,8,3,7, 2));
+    public static Set<Integer> numbers = new HashSet<>(numbersList);
+
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 1, 2, 3, 4, 5, 1, 2, 1,8,3,7, 2);
-        // Create a HashSet to store unique elements
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        // Print each unique number along with its occurrences
-        printUniqueNumbersWithCount(numbers);
-    }
-    // Function to print each unique number along with its occurrences
-    private static void printUniqueNumbersWithCount(List<Integer> numbers) {
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-        for (Integer number : uniqueNumbers) {
-            int occurrences = countOccurrences(numbers, number);
-            System.out.println("Number: " + number + ", Occurrences: " + occurrences);
+        for (Integer x : numbers) {
+            System.out.println(x + " has : " + occurrences(x) + " occurrences in list");
         }
     }
-    // Helper function to count occurrences of each  a number in the list
-    private static int countOccurrences(List<Integer> numbers, int target) {
+
+    public static Integer occurrences(Integer numberToFind) {
         int count = 0;
-        for (Integer number : numbers) {
-            if (number == target) {
+        for (Integer number : numbersList) {
+            if (number.equals(numberToFind)) {
                 count++;
             }
         }
         return count;
     }
-}
+    }
